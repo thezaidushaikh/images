7517d5c40b12011e9bf995301b0f6100f0b8ecee
Zaid <zaidus800@gmail.com>
2024-04-26 15:11:56 +0500

    image error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="image">
-        <img src="./public/images/Fail Big Dream Big.jpg" alt="" />
+        <img src="./images/Fail Big Dream Big.jpg" alt="Fail Big Dream Big" />
       </div>
+      <a href="./docs/Zaid's Resume.pdf" download={"zaid's.pdf"}>
+        <div className="download"><p>Download</p></div>
+      </a>
     </>
   );
 }
 
-export default App
+export default App;
